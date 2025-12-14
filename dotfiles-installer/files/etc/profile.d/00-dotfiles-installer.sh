bde7a802ddd933a11614baf24faa2bf7dd586b9a
Dankcuddlybear <dankcuddlybear@proton.me>
2025-12-14 01:19:07 +0000

    Bug fixes

--- dotfiles-installer/files/etc/profile.d/00-dotfiles-installer.sh
+++ dotfiles-installer/files/etc/profile.d/00-dotfiles-installer.sh
@@ -4,11 +4,10 @@ if [ ! -f "$HOME/.config/dotfiles-installer" ]; then
 		root) DOTFILES_DIR="/etc/dotfiles.d/root";;
 		*) DOTFILES_DIR="/etc/dotfiles.d/user";;
 	esac
-	## Create missing directories if root
-	[ ! -d "$DOTFILES_DIR" ] && [ $USER == root ] && mkdir -p /etc/dotfiles.d/root /etc/dotfiles.d/user "$DOTFILES_DIR"
 	## Install dotfiles to user home directory, if the dotfiles directory exists
-	if [ -d "$DOTFILES_DIR" ]; then
-		cp -r "$DOTFILES_DIR/*" "$HOME/"; cp -r "$DOTFILES_DIR/.*" "$HOME/"
+	CDIR="$PWD"; if [ -d "$DOTFILES_DIR" ]; then
+		cd "$DOTFILES_DIR"
+		cp -r "*" "$HOME/"; cp -r ".*" "$HOME/"
 		mkdir -p "$HOME/.config"; touch "$HOME/.config/dotfiles-installer"
 	fi
 fi
